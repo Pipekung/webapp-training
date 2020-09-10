e73dbcb804f03ecf2e2e2ef93f6afed27295646b
PK <chanja@kku.ac.th>
2020-09-10 18:36:57 +0700

    ..

--- run-php-fpm.sh
+++ run-php-fpm.sh
@@ -1,5 +1,8 @@
 docker rm -f php-fpm
 docker run -d \
-    -- name php-fpm \
+    --name php-fpm \
     -v /c/web/web4/:/var/www/html \
     php:7.4-fpm-alpine
+docker exec -it php-fpm docker-php-ext-install pdo_mysql
+docker exec -it php-fpm docker-php-ext-enable pdo_mysql
+docker restart php-fpm
