2fe3a048fa809ec767916f7b6b0fc2837e131474
PK <chanja@kku.ac.th>
2020-09-10 18:14:10 +0700

    ..

--- run-nginx.sh
+++ run-nginx.sh
@@ -1,5 +1,6 @@
 docker rm -f nginx
 docker run -d \
+    -- name nginx \
     -p 8084:80 \
     -v /c/web/web4/:/var/www/html \
     -v /c/web/web4/nginx/conf.d/:/etc/nginx/conf.d/ \
--- run-php-fpm.sh
+++ run-php-fpm.sh
@@ -1,4 +1,5 @@
 docker rm -f php-fpm
 docker run -d \
+    -- name php-fpm \
     -v /c/web/web4/:/var/www/html \
     php:7.4-fpm-alpine
