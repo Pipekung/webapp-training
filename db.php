13913c9226f3a54bd9c4b76bf7e2cae24b4660da
Channarong Janpanich <cj.pipekung@gmail.com>
2020-09-11 14:13:42 +0700

    .

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$dns="mysql:host=192.168.99.100;dbname=web;charset=utf8";
+$dns="mysql:host=127.0.0.1;port=3333;dbname=web;charset=utf8";
 $username="root";
 $password="1234";
 
--- run-php-fpm.sh
+++
@@ -1,8 +0,0 @@
-docker rm -f php-fpm
-docker run -d \
-    --name php-fpm \
-    -v /c/web/web4/:/var/www/html \
-    php:7.4-fpm-alpine
-docker exec -it php-fpm docker-php-ext-install pdo_mysql
-docker exec -it php-fpm docker-php-ext-enable pdo_mysql
-docker restart php-fpm
